5d9d3c7825a0d644437535ef2161acf7b9190b36
diguie_t <timothy.diguiet@epitech.eu>
2018-03-01 20:52:39 +0100

    iterate over input files args

--- main.cpp
+++ main.cpp
@@ -14,19 +14,21 @@ int main(int ac, char **av) {
 		std::cerr << "No file name given" << std::endl;
 		return EXIT_FAILURE;
 	}
-	auto file = Utils::readFile(av[1]);
-	auto split = Utils::split(file, "\n");
-	if (split.size() < 2) {
-		std::cerr << "Invalid simulation parameters" << std::endl;
-		return EXIT_FAILURE;
-	}
-	SimulationInfo::init(split[0]);
-	Rides rides = Rides(std::vector<std::string>(split.begin() + 1, split.end()));
-//	std::cout << rides.toString() << std::endl;
+	for (int i = 1 ; i < ac ; i++) {
+		auto file = Utils::readFile(av[i]);
+		auto split = Utils::split(file, "\n");
+		if (split.size() < 2) {
+			std::cerr << "Invalid simulation parameters" << std::endl;
+			return EXIT_FAILURE;
+		}
+		SimulationInfo::init(split[0]);
+		Rides rides = Rides(std::vector<std::string>(split.begin() + 1, split.end()));
+		//	std::cout << rides.toString() << std::endl;
 
-	Schedule schedule(SimulationInfo::nbVehicles);
-	schedule.compute(&rides);
-	schedule.computeOutput(Utils::split(av[1], ".")[0] + ".out");
+		Schedule schedule(SimulationInfo::nbVehicles);
+		schedule.compute(&rides);
+		schedule.computeOutput(Utils::split(av[i], ".")[0] + ".out");
+	}
 
 	return EXIT_SUCCESS;
 }
