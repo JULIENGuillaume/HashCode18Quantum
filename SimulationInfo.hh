71b35ab7ade34800bd5bc79706c9d4b0fe9cce5b
diguie_t <timothy.diguiet@epitech.eu>
2018-03-01 19:24:53 +0100

    added simulation info class

---
+++ SimulationInfo.hh
@@ -0,0 +1,18 @@
+//
+// Created by diguie_t on 3/1/2018.
+//
+
+#ifndef QUANTUMHASHCODE18_SIMULATIONINFO_HH
+#define QUANTUMHASHCODE18_SIMULATIONINFO_HH
+
+class SimulationInfo {
+public:
+	static unsigned int nbX = 0;
+	static unsigned int nbY = 0;
+	static unsigned int nbVehicles = 0;
+	static unsigned int nbRides = 0;
+	static unsigned int startBonus = 0;
+	static unsigned int nbSteps = 0;
+};
+
+#endif //QUANTUMHASHCODE18_SIMULATIONINFO_HH
--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 // Created by Guillaume Julien on 01/03/2018.
 //
 
+#include "SimulationInfo.hh"
+
 int main() {
+	SimulationInfo::nbX = 0;
+	SimulationInfo::nbY = 0;
+	SimulationInfo::nbVehicles = 0;
+	SimulationInfo::nbRides = 0;
+	SimulationInfo::startBonus = 0;
+	SimulationInfo::nbSteps = 0;
 
 }
